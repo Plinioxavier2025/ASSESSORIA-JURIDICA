a2ad32ccd307386d1c070c2ecf9fcde89859e23d
plinio <xavier.plinio@gmail.com>
2026-06-16 09:24:19 -0300

    ci: adicionar configuracao do vite e atualizar build

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // Define caminhos relativos para os assets (funciona em qualquer subdiretório como o GitHub Pages)
+});
